049288d478bb52f759be6f37fa38d621309e952d
abarcaEs3 <abarcaespinozacarlos@gmail.com>
2026-08-14 15:39:26 -0600

    Date Pipe

--- src/app/pages/basic-pages/basic-pages.component.ts
+++ src/app/pages/basic-pages/basic-pages.component.ts
@@ -1,9 +1,9 @@
-import { LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
-import { Component, signal } from '@angular/core';
+import { DatePipe, LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
+import { Component, effect, signal } from '@angular/core';
 
 @Component({
   selector: 'app-basic-pages',
-  imports: [LowerCasePipe, UpperCasePipe, TitleCasePipe],
+  imports: [LowerCasePipe, UpperCasePipe, TitleCasePipe, DatePipe],
   templateUrl: './basic-pages.component.html',
 })
 export default class BasicPageComponent {
@@ -11,4 +11,16 @@ export default class BasicPageComponent {
   nameLower = signal('carlos');
   nameUpper = signal('CARLOS');
   fullName = signal('cArLOs AbARcA');
+
+  customDate = signal(new Date());
+
+  tickingDateEffect = effect((onCleanDate) => {
+    const interval = setInterval(() => {
+      this.customDate.set(new Date());
+    }, 1000);
+
+    onCleanDate(() => {
+      clearInterval(interval);
+    })
+  })
 }
