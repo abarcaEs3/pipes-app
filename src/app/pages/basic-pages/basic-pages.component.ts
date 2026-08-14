a7e817526f84669df544684bf52118bfe5aa54b8
abarcaEs3 <abarcaespinozacarlos@gmail.com>
2026-08-14 14:16:26 -0600

    UpperCase, LowerCase y TitleCase Pipes

--- src/app/pages/basic-pages/basic-pages.component.ts
+++ src/app/pages/basic-pages/basic-pages.component.ts
@@ -1,8 +1,14 @@
-import { Component } from '@angular/core';
+import { LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-basic-pages',
-  imports: [],
+  imports: [LowerCasePipe, UpperCasePipe, TitleCasePipe],
   templateUrl: './basic-pages.component.html',
 })
-export default class BasicPageComponent {}
+export default class BasicPageComponent {
+
+  nameLower = signal('carlos');
+  nameUpper = signal('CARLOS');
+  fullName = signal('cArLOs AbARcA');
+}
